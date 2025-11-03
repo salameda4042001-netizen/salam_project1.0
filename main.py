2c2ed280d61531db7d05615cc42f5962217bb00f
salameda4042001-netizen <salameda4042001@gmail.com>
2025-11-03 13:13:33 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import streamlit as st
 st.title('Salam Project')
 password=st.text_input('암호을 임력하세요.')
+st.selectbox('whgdkgksms dmatlrdmf tjsxorgo wntpdy:',['clzls',vlwk'])
 if st.button('button'):
   st.info(password+'님 안녕하세요.')
   st.warning('반갑습니다.')
+  st.error('dlqslek.)
   st.balloons()
