d98a4118b9f8986501402ddc3cf4ef6114ab454f
salameda4042001-netizen <salameda4042001@gmail.com>
2025-11-03 13:15:20 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 st.title('Salam Project')
 password=st.text_input('암호을 임력하세요.')
-st.selectbox('whgdkgksms dmatlrdmf tjsxorgo wntpdy:',['clzls','vlwk'])
+st.selectbox('선택하세요:',['clzls','vlwk'])
 if st.button('button'):
   st.info(password+'님 안녕하세요.')
   st.warning('반갑습니다.')
